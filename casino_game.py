3651d96028f5460f1e65253fbc3e50db6eb5279d
Will Thompson <thompsonwill302@gmail.com>
2024-10-17 21:34:39 -0700

    FIRST COMMIT

---
+++ casino_game.py
@@ -0,0 +1,5 @@
+import random
+
+##This game has a player getting to play some classic casino games. In these games they will get to bet and choose between multiple options per hand. Blackjack roulette and slots are the planned games.
+
+class Player:
